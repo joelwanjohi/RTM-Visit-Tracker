9444b6e3c8b6698eedc13b8b32e96db5850e664e
joelwanjohi <joelwanjohi207@gmail.com>
2025-05-17 19:35:16 +0300

    connectivity, null-aware

--- lib/features/customers/presentation/bloc/customer_bloc.dart
+++ lib/features/customers/presentation/bloc/customer_bloc.dart
@@ -1,22 +1,22 @@
-// import 'package:bloc/bloc.dart';
-// import 'package:equatable/equatable.dart';
-// import 'package:rtm_visit_tracker/features/customers/domain/entities/customer.dart';
-// import 'package:rtm_visit_tracker/features/customers/domain/usecases/get_customers.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rtm_visit_tracker/features/customers/domain/entities/customer.dart';
+import 'package:rtm_visit_tracker/features/customers/domain/usecases/get_customers.dart';
 
-// part 'customer_event.dart';
-// part 'customer_state.dart';
+part 'customer_event.dart';
+part 'customer_state.dart';
 
-// class CustomerBloc extends Bloc<CustomerEvent, CustomerState> {
-//   final GetCustomers getCustomers;
+class CustomerBloc extends Bloc<CustomerEvent, CustomerState> {
+  final GetCustomers getCustomers;
 
-//   CustomerBloc({required this.getCustomers}) : super(CustomerInitial()) {
-//     on<FetchCustomers>((event, emit) async {
-//       emit(CustomerLoading());
-//       final result = await getCustomers();
-//       result.fold(
-//         (failure) => emit(CustomerError(message: failure.message)),
-//         (customers) => emit(CustomerLoaded(customers)),
-//       );
-//     });
-//   }
-// }
+  CustomerBloc({required this.getCustomers}) : super(CustomerInitial()) {
+    on<FetchCustomers>((event, emit) async {
+      emit(CustomerLoading());
+      final result = await getCustomers();
+      result.fold(
+        (failure) => emit(CustomerError(message: failure.message)),
+        (customers) => emit(CustomerLoaded(customers)),
+      );
+    });
+  }
+}
