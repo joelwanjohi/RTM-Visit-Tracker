042344ddd13130dcf2140de845b7a0af5dbe5c4a
joelwanjohi <joelwanjohi207@gmail.com>
2025-05-17 19:32:46 +0300

    connectivity, null-aware

--- lib/features/statistics/presentation/bloc/statistics_bloc.dart
+++ lib/features/statistics/presentation/bloc/statistics_bloc.dart
@@ -1,24 +1,24 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rtm_visit_tracker/features/statistics/domain/entities/statistics.dart';
-import 'package:rtm_visit_tracker/features/statistics/domain/usecases/get_statistics.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:rtm_visit_tracker/features/statistics/domain/entities/statistics.dart';
+// import 'package:rtm_visit_tracker/features/statistics/domain/usecases/get_statistics.dart';
 
-part 'statistics_event.dart';
-part 'statistics_state.dart';
+// part 'statistics_event.dart';
+// part 'statistics_state.dart';
 
-class StatisticsBloc extends Bloc<StatisticsEvent, StatisticsState> {
-  final GetStatistics getStatistics;
+// class StatisticsBloc extends Bloc<StatisticsEvent, StatisticsState> {
+//   final GetStatistics getStatistics;
 
-  StatisticsBloc({required this.getStatistics}) : super(StatisticsInitial()) {
-    on<FetchStatistics>(_onFetchStatistics);
-  }
+//   StatisticsBloc({required this.getStatistics}) : super(StatisticsInitial()) {
+//     on<FetchStatistics>(_onFetchStatistics);
+//   }
 
-  Future<void> _onFetchStatistics(FetchStatistics event, Emitter<StatisticsState> emit) async {
-    emit(StatisticsLoading());
-    final result = await getStatistics();
-    result.fold(
-      (failure) => emit(StatisticsError(failure.message)),
-      (statistics) => emit(StatisticsLoaded(statistics)),
-    );
-  }
-}
+//   Future<void> _onFetchStatistics(FetchStatistics event, Emitter<StatisticsState> emit) async {
+//     emit(StatisticsLoading());
+//     final result = await getStatistics();
+//     result.fold(
+//       (failure) => emit(StatisticsError(failure.message)),
+//       (statistics) => emit(StatisticsLoaded(statistics)),
+//     );
+//   }
+// }
